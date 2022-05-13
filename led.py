e8b15c88d835952462da3d455a326d25704972b1
Flurin Feuerstein <flufeu@gmail.com>
2022-05-13 20:30:26 +0200

    should work :-P

--- led.py
+++ led.py
@@ -2,24 +2,41 @@ import machine
 import utime
 
 humidityThreshold = 20000
-led = machine.Pin(20, machine.Pin.OUT)
-humiditySensor = machine.ADC(26)
+led = machine.Pin(13, machine.Pin.OUT)
 
-def ReadHumidity():
+out19 = machine.Pin(19, machine.Pin.OUT)
+out20 = machine.Pin(20, machine.Pin.OUT)
+
+in26 = machine.ADC(26)
+in27 = machine.ADC(27)
+
+pots = [
+    ( in26, out19 ),
+    ( in27, out20 ),
+]
+
+def ReadHumidity(humiditySensor):
     return humiditySensor.read_u16()
-
-def ToggleLed():
-    led.toggle()
 
 def CheckPumpEnablingNecessary(humidity):
     return humidity < humidityThreshold
 
-def SetPumpState(enable):
-    led.value(enable)
+def SetPumpState(pump, enable):
+    pump.value(enable)
 
 while True:
     utime.sleep(1)
-    humidity = ReadHumidity()
-    enablePumpNecessary = CheckPumpEnablingNecessary(humidity)
-    SetPumpState(enablePumpNecessary)
+    led.Togge()
+    for pot in pots:
+        inPin = pot[0]
+        outPin = pot[1]
+        humidity = ReadHumidity(inPin)
+        enablePumpNecessary = CheckPumpEnablingNecessary(humidity)
+        SetPumpState(outPin, enablePumpNecessary)
+        print(humidity)
+        print(enablePumpNecessary)
+        print()
+    print("-------------------------------------") 
 
+
+#7:15
